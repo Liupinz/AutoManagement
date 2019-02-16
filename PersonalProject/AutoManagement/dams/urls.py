6915188dbacdd023d98978c7c2f698ded6df6456
Liupinz <2663480697@qq.com>
2019-02-17 01:06:06 +0800

    add function

--- PersonalProject/AutoManagement/dams/urls.py
+++ PersonalProject/AutoManagement/dams/urls.py
@@ -3,4 +3,6 @@ from dams import views
 
 urlpatterns = [
     url(r'^login$', views.login),
+    url(r'^dashboard$', views.dashboard),
+    url(r'^logincheck$', views.logincheck),
 ]
--- PersonalProject/AutoManagement/dams/views.py
+++ PersonalProject/AutoManagement/dams/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from dams.models import User
 
 # Create your views here.
 
 def login(request):
     return render(request, 'dams/login.html')
+
+def logincheck(request):
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    userResult = User.objects.filter(uname=username, upassword=password)
+    if (len(username) > 0):
+        return redirect('/dashboard')
+    else:
+        return redirect('/login')
+
+def dashboard(request):
+    return render(request, 'dams/dashboard.html')
+
+
+
+
+
+
+
+
+
+
 
